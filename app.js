ab173e24101307306a75dff1c01aaeb4f9d47089
akhilmallepally <akhilnani831@gmail.com>
2021-03-31 23:07:30 -0500

    added stars pug and js files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helicopterRouter = require('./routes/helicopter')
+var starsRouter = require('./routes/stars')
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/helicopter', helicopterRouter);
+app.use('/stars', starsRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
