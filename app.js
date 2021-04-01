6d78b715f17467f75c48ad48d72a1483df94289e
akhilmallepally <akhilnani831@gmail.com>
2021-03-31 23:13:27 -0500

    added slot pug and js files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helicopterRouter = require('./routes/helicopter')
 var starsRouter = require('./routes/stars')
+var slotRouter = require('./routes/slot')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/helicopter', helicopterRouter);
 app.use('/stars', starsRouter)
+app.use('/slot', slotRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
